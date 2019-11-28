9c9d00fb7959b0320ee3464d12361b9748febdbe
d21bhardwaj <39788819+d21bhardwaj@users.noreply.github.com>
2019-11-28 23:06:21 +0530

    Add files via upload

--- entryLog/entryLog/settings.py
+++ entryLog/entryLog/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "email@gmail.com"
+# EMAIL_HOST_PASSWORD = 'Google App key'
+# DEFAULT_FROM_EMAIL = 'Test <EMAIL_HOST_USER>'
